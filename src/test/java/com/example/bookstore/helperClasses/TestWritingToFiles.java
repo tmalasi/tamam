c2d1346a34870bfd74867aa659a9d74a9d2f92f2
Tea Malasi <tmalasi21@epoka.edu.al>
2023-12-14 19:10:33 +0100

    Added back what i unintentionally removed

--- src/test/java/com/example/bookstore/helperClasses/TestWritingToFiles.java
+++ src/test/java/com/example/bookstore/helperClasses/TestWritingToFiles.java
@@ -1,26 +1,16 @@
 package com.example.bookstore.helperClasses;
-
-
-import com.example.bookstore.Controllers.Controller;
 import com.example.bookstore.Models.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
-
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.Scanner;
-
 import static org.junit.jupiter.api.Assertions.*;
-
-
 class TestWritingToFiles {
-
     @Test
     public void testReadCredentials_CheckFileIsCreatedCorrectlly() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -48,13 +38,10 @@ class TestWritingToFiles {
         // Read data from the temporary file and find the role
         String username = "SampleAddress";
         String password = "SamplePhone";
-
         String role = writingToFiles.readCredentials(username, password, tempFile.toString());
         assertEquals("Librarian", role);
         tempFile.deleteOnExit();
     }
-
-
     @Test
     public void testReadCredentials_NonValidUsername() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -68,12 +55,10 @@ class TestWritingToFiles {
         // Read data from the temporary file and find the role
         String username = "SampleAddresss";
         String password = "SamplePhone";
-
         String role = writingToFiles.readCredentials(username, password, tempFile.toString());
         assertNull(role);
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testReadCredentials_NonValidPassword() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -87,7 +72,6 @@ class TestWritingToFiles {
         // Read data from the temporary file and find the role
         String username = "SampleAddress";
         String password = "SamplePhoneee";
-
         String role = writingToFiles.readCredentials(username, password, tempFile.toString());
         assertNull(role);
         tempFile.deleteOnExit();
@@ -102,7 +86,6 @@ class TestWritingToFiles {
         assertNull(role);
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testReadCredentials_NullCredentials() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -117,13 +100,11 @@ class TestWritingToFiles {
         assertNull(role);
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testReadCredentials_ExceptionThrown() {
         String role = writingToFiles.readCredentials(null, null, null);
         assertNull(role);
     }
-
     @Test
     public void testWriteRoles_CheckFileIsCreatedCorrectlly() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -132,7 +113,6 @@ class TestWritingToFiles {
         assertFileExistsAndNotEmpty(Path.of(tempFile.toString()));
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testWriteRoles_CheckContentOfFileWhenYouDontWriteAnything() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -141,7 +121,6 @@ class TestWritingToFiles {
         String fileContent= readFileContents(tempFile.toString());
         assertEquals("admin,admin,Administrator\n", fileContent);
         tempFile.deleteOnExit();
-
     }
     @Test
     public void testWriteRoles_CheckContentOfFile() throws IOException {
@@ -157,18 +136,14 @@ class TestWritingToFiles {
                 "SampleAddress,SamplePhone,Librarian\n" +
                 "Author 2,2024,Manager\n", fileContent);
         tempFile.deleteOnExit();
-
     }
-
     @Test
     public void testWriteRoles_ThrowException() {
         ArrayList<Person> personArrayList= new ArrayList<>();
         assertThrows(RuntimeException.class, () -> {
             writingToFiles.writeRoles(null, personArrayList);
         });
-
     }
-
     @Test
     public void testGetBooks_CheckFileIsCreatedCorrectllyButIsEmpty() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -176,7 +151,6 @@ class TestWritingToFiles {
         assertEquals(books, writingToFiles.getBooks(tempFile.toString()));
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetBooks_ObservableListNotNull() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -190,7 +164,6 @@ class TestWritingToFiles {
         assertNotNull(writingToFiles.getBooks(tempFile.toString()).size());
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetBooks_ContainsTheCorrectNumberOfBooks() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -204,7 +177,6 @@ class TestWritingToFiles {
         Assertions.assertEquals(2,writingToFiles.getBooks(tempFile.toString()).size());
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetBooks_AttributesOfTheBookCorrectlyPopulated() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -214,7 +186,7 @@ class TestWritingToFiles {
             writer.write(book1 + "\n");
             writer.write(book2+"\n");
             writer.close();
-           }
+        }
         ObservableList<Book> books = writingToFiles.getBooks(tempFile.toString());
         Book firstBook = books.getFirst();
         assertEquals("NewBook1",firstBook.getTitle());
@@ -222,20 +194,14 @@ class TestWritingToFiles {
         assertEquals("Comedy",firstBook.getCategory());
         assertEquals("12345",firstBook.getIsbn());
         assertEquals(34,firstBook.getStock());
-
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetBooks_ThrowException() {
         assertThrows(RuntimeException.class, () -> {
             writingToFiles.getBooks(null);
         });
-
     }
-
-
-
     @Test
     public void testGetPersons_CheckFileIsCreatedCorrectllyButIsEmpty() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -243,7 +209,6 @@ class TestWritingToFiles {
         assertEquals(people, writingToFiles.getPersons(tempFile.toString()));
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetPersons_ObservableListNotNull() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -257,7 +222,6 @@ class TestWritingToFiles {
         assertNotNull(writingToFiles.getPersons(tempFile.toString()));
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetPersons_ContainsTheCorrectNumberOfPersons() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -271,7 +235,6 @@ class TestWritingToFiles {
         Assertions.assertEquals(2,writingToFiles.getPersons(tempFile.toString()).size());
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetPersons_AttributesOfTheBookCorrectlyPopulated() throws IOException {
         File tempFile = File.createTempFile( "prefix", "txt");
@@ -289,10 +252,8 @@ class TestWritingToFiles {
         assertEquals("SamplePhone",firstPerson.getPassword());
         assertEquals(30,firstPerson.getSalary());
         assertEquals(Role.Librarian,firstPerson.getRole());
-
         tempFile.deleteOnExit();
     }
-
     @Test
     public void testGetPersons_ThrowException() {
         assertThrows(RuntimeException.class, () -> {
@@ -302,28 +263,59 @@ class TestWritingToFiles {
     }
 
     @Test
+    public void testGetNumberOfLibrarians_inWrittingToFilesButIsEmptyList(){
+        ArrayList<Person> people = new ArrayList<>();
+        assertEquals("0", writingToFiles.getNumberOfLibrarians(people));
+    }
+
+    @Test
     public void testGetNumberOfLibrarians_inWrittingToFiles(){
-//        String numberOfLibrarians = writingToFiles.getNumberOfLibrarians( );
-//        assertEquals("4",numberOfLibrarians);
+        ArrayList<Person> people = new ArrayList<>();
+        Person person1 = new Librarian("SampleName", "SampleAddress", "SamplePhone", "SampleEmail", 30, "SampleUsername", Role.Librarian, 100.0);
+        Person person2 = new Manager("Book 2", "Author 2", "2024", "9202-2-2-", 2323, "sllwlwls", Role.Manager);
+
+        people.add(person1);
+        people.add(person2);
+
+        assertEquals("1", writingToFiles.getNumberOfLibrarians(people));
     }
 
 
+    @Test
+    public void testGetNumberOfManagers_inWrittingToFilesButIsEmptyList(){
+        ArrayList<Person> people = new ArrayList<>();
+        assertEquals("0", writingToFiles.getNumberOfManagers(people));
+    }
     @Test
     public void testGetNumberOfManagers_inWrittingToFiles(){
-        ArrayList<Person> arrayList = new ArrayList<Person>(Controller.people);
-        String numberOfManagers = writingToFiles.getNumberOfManagers(arrayList);
-        assertEquals("2", numberOfManagers);
-    }
+        ArrayList<Person> people = new ArrayList<>();
+        Person person1 = new Librarian("SampleName", "SampleAddress", "SamplePhone", "SampleEmail", 30, "SampleUsername", Role.Librarian, 100.0);
+        Person person2 = new Manager("Book 2", "Author 2", "2024", "9202-2-2-", 2323, "sllwlwls", Role.Manager);
 
+        people.add(person1);
+        people.add(person2);
+
+        assertEquals("1", writingToFiles.getNumberOfManagers(people));
+    }
     @Test
-    public void testGetNumberOfBills_InTheExistingDirectory() {
-        int numberOfBills = writingToFiles.getNumberOfBills("res/Bills");
-        assertEquals(25, numberOfBills);
+    public void testGetNumberOfBills_WhenWePassAFileNotADirectroy_ThrowsException () throws IOException {
+        File tempFile = File.createTempFile( "prefix", "txt");
+        assertThrows(RuntimeException.class, () -> {
+            writingToFiles.getNumberOfBills(tempFile.toString());
+        });
+        tempFile.deleteOnExit();
     }
     @Test
     public void testGetNumberOfBills_InTheNONExistingDirectory() {
         int numberOfBills = writingToFiles.getNumberOfBills("path/to/non-existing-directory");
         assertEquals(0, numberOfBills);
+    }
+
+    @Test
+    public void testGetNumberOfBills_InTheExistingDirectoryIfBiggerThen0ThenCorrectNumberOfFiles(){
+        int numberOfBills=writingToFiles.getNumberOfBills("res/Bills");
+        boolean isBiggerThen0= (numberOfBills>0);
+        assertTrue(isBiggerThen0);
     }
 
     @Test
@@ -334,26 +326,21 @@ class TestWritingToFiles {
         assertTrue(new File(expectedFilePath).exists());
         deleteFile(expectedFilePath);
     }
-
-    
     @Test
     public void testGetTotalBill_forNONExistingPath(){
         double totalBill=writingToFiles.getTotalBill("path/to/non-existing-bill");
         assertEquals(0.0,totalBill);
     }
-
     @Test
     public void testGetTotalCost_forNONExistingPath(){
         double totalCost=writingToFiles.getTotalCost("path/to/non-existing-bill");
         assertEquals(0.0,totalCost);
     }
-
     @Test
     public void testGetBooksSold_forNONExistingPath(){
         double booksSold =writingToFiles.getTotalCost("path/to/non-existing-bill");
         assertEquals(0, booksSold);
     }
-
     //fix this
 //    @Test
 //    public void testWriteBooks_ContainsTheRightInformation(){
@@ -373,7 +360,6 @@ class TestWritingToFiles {
 //        String fileContent = readFileContents("res/persons.txt");
 //        assertTrue(fileContent.contains(samplePerson.toString()), "File should contain the sample person information");
 //    }
-
     @Test
     public void testWriteTotalBill_OfExistingFile() {
         double sampleTotal = 1165.32;
@@ -388,22 +374,18 @@ class TestWritingToFiles {
         double actualTotalCost = readTotal("res/totalCost.bin");
         assertEquals(sampleTotalCost, actualTotalCost, "File should contain the sample total cost");
     }
-
     private double readTotal(String filepath) {
-            File file = new File(filepath);
-            if (file.exists()) {
-                try (FileInputStream fis = new FileInputStream(file);
-                     DataInputStream dis = new DataInputStream(fis)) {
-                    return dis.readDouble();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+        File file = new File(filepath);
+        if (file.exists()) {
+            try (FileInputStream fis = new FileInputStream(file);
+                 DataInputStream dis = new DataInputStream(fis)) {
+                return dis.readDouble();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
             }
-            return 0.0; // Return 0 if the file does not exist
         }
-
-
-
+        return 0.0; // Return 0 if the file does not exist
+    }
     private String readFileContents(String filePath) {
         StringBuilder content = new StringBuilder();
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
@@ -422,7 +404,6 @@ class TestWritingToFiles {
             System.err.println("Failed to delete the temporary file: " + filePath);
         }
     }
-
     private void deleteFile(Path filePath) {
         try {
             Files.deleteIfExists(filePath);
@@ -431,7 +412,6 @@ class TestWritingToFiles {
             e.printStackTrace();
         }
     }
-
     private void assertFileContent(Path tempFile, String expectedContent) {
         try {
             String fileContent = Files.readString(tempFile);
@@ -440,11 +420,9 @@ class TestWritingToFiles {
             throw new RuntimeException(e);
         }
     }
-
     private Path createTempFilePath() throws IOException {
         return Files.createTempFile("tempFile", ".txt");
     }
-
     private void assertFileExistsAndNotEmpty(Path filePath) {
         assertTrue(Files.exists(filePath), "File should exist");
         assertTrue(Files.isRegularFile(filePath), "Should be a regular file");
@@ -454,5 +432,4 @@ class TestWritingToFiles {
             throw new RuntimeException(e);
         }
     }
-
 }
