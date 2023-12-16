58f836676588c5c07ff8c106b5b6449cf93e73a5
Kevin Llaca <127748914+kllaca@users.noreply.github.com>
2023-12-17 00:05:01 +0100

    Tests for the Librarian Module 100% coverage

--- src/test/java/com/example/bookstore/Models/LibrarianTest.java
+++ src/test/java/com/example/bookstore/Models/LibrarianTest.java
@@ -1,7 +1,35 @@
 package com.example.bookstore.Models;
 
+import com.example.bookstore.helperClasses.Role;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class LibrarianTest {
+    @Test
+    public void testSetTotalBilledNegativeValue() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            Librarian librarian=new Librarian("TestName","TestUsername","TestPasword","01-01-2002",400,"+3555555",Role.Librarian,-1);
+            librarian.setTotalBilled(librarian.getTotalBilled());
+        });
+    }
+    @Test
+    public void testSetTotalBilledValuePositive() {
+        Librarian librarian=new Librarian("TestName","TestUsername","TestPasword","01-01-2002",400,"+3555555",Role.Librarian,20);
+        librarian.setTotalBilled(librarian.getTotalBilled());
+        assertEquals(20, librarian.getTotalBilled());
+    }
+
+    @Test
+    public void testTheConstructorWithoutTotalBilledToStringMethod() {
+        Librarian librarian=new Librarian("TestName","TestUsername","TestPassword","01-01-2002",400,"+3555555",Role.Librarian);
+        assertEquals("TestName,01-01-2002,+3555555,400,TestUsername,TestPassword,Librarian,0.0",librarian.toString());
+
+    }
+    @Test
+    void testPersonToStringMethod() {
+        Librarian librarian= new Librarian("TestName","TestUsername","TestPassword","01-01-2002",233,"+355686767", Role.Librarian,30);
+        assertEquals("TestName,01-01-2002,+355686767,233,TestUsername,TestPassword,Librarian,30.0",librarian.toString());
+    }
 
 }
